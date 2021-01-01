08802b084a6751283d8b7169c81d67a1efedd3f9
Elliott Chenger <elliott@omitneedlesscode.com>
2021-01-01 12:32:59 -0600

    Adding html for MVP module

--- MagicMirrorModule-Oura.js
+++ MagicMirrorModule-Oura.js
@@ -9,110 +9,62 @@
 
 Module.register("MagicMirrorModule-Oura", {
 	defaults: {
-		updateInterval: 60000,
-		retryDelay: 5000
 	},
 
 	requiresVersion: "2.1.0", // Required version of MagicMirror
 
-	start: function() {
-		var self = this;
-		var dataRequest = null;
-		var dataNotification = null;
-
-		//Flag for check if module is loaded
-		this.loaded = false;
-
-		// Schedule update timer.
-		this.getData();
-		setInterval(function() {
-			self.updateDom();
-		}, this.config.updateInterval);
-	},
-
-	/*
-	 * getData
-	 * function example return data and show it in the module wrapper
-	 * get a URL request
-	 *
-	 */
-	getData: function() {
-		var self = this;
-
-		var urlApi = "https://jsonplaceholder.typicode.com/posts/1";
-		var retry = true;
-
-		var dataRequest = new XMLHttpRequest();
-		dataRequest.open("GET", urlApi, true);
-		dataRequest.onreadystatechange = function() {
-			console.log(this.readyState);
-			if (this.readyState === 4) {
-				console.log(this.status);
-				if (this.status === 200) {
-					self.processData(JSON.parse(this.response));
-				} else if (this.status === 401) {
-					self.updateDom(self.config.animationSpeed);
-					Log.error(self.name, this.status);
-					retry = false;
-				} else {
-					Log.error(self.name, "Could not load data.");
-				}
-				if (retry) {
-					self.scheduleUpdate((self.loaded) ? -1 : self.config.retryDelay);
-				}
-			}
-		};
-		dataRequest.send();
-	},
-
-
-	/* scheduleUpdate()
-	 * Schedule next update.
-	 *
-	 * argument delay number - Milliseconds before next update.
-	 *  If empty, this.config.updateInterval is used.
-	 */
-	scheduleUpdate: function(delay) {
-		var nextLoad = this.config.updateInterval;
-		if (typeof delay !== "undefined" && delay >= 0) {
-			nextLoad = delay;
-		}
-		nextLoad = nextLoad ;
-		var self = this;
-		setTimeout(function() {
-			self.getData();
-		}, nextLoad);
-	},
-
 	getDom: function() {
 		var self = this;
+		//Scores Row
+		var scoresRow = document.createElement("tr")
+		scoresRow.className = "oura-scores"
+		
+		//Readiness Score
+		var readinessScoreContainer = document.createElement("th")
+		var readinessScore = document.createTextNode("93")
+		readinessScoreContainer.appendChild(readinessScore)
+		scoresRow.appendChild(readinessScoreContainer)
 
-		// create element wrapper for show into the module
-		var wrapper = document.createElement("div");
-		// If this.dataRequest is not empty
-		if (this.dataRequest) {
-			var wrapperDataRequest = document.createElement("div");
-			// check format https://jsonplaceholder.typicode.com/posts/1
-			wrapperDataRequest.innerHTML = this.dataRequest.title;
+		// Sleep Score
+		var sleepScoreContainer = document.createElement("th")
+		var sleepScore = document.createTextNode("93")
+		sleepScoreContainer.appendChild(sleepScore)
+		scoresRow.appendChild(sleepScoreContainer)
 
-			var labelDataRequest = document.createElement("label");
-			// Use translate function
-			//             this id defined in translations files
-			labelDataRequest.innerHTML = this.translate("TITLE");
+		// Sleep Duration
+		var sleepDurationContainer = document.createElement("th")
+		var sleepDuration = document.createTextNode("8h 2m")
+		sleepDurationContainer.appendChild(sleepDuration)
+		scoresRow.appendChild(sleepDurationContainer)
 
+		//Labels Row
+		var labelsRow = document.createElement("tr")
+		labelsRow.className = "oura-lables"
+		
+		//Readiness Label
+		var readinessLabelContainer = document.createElement("th")
+		var readinessLabel = document.createTextNode("Readiness")
+		readinessLabelContainer.appendChild(readinessLabel)
+		labelsRow.appendChild(readinessLabelContainer)
 
-			wrapper.appendChild(labelDataRequest);
-			wrapper.appendChild(wrapperDataRequest);
-		}
+		//Sleep Label
+		var sleepLabelContainer = document.createElement("th")
+		var sleepLabel = document.createTextNode("Sleep")
+		sleepLabelContainer.appendChild(sleepLabel)
+		labelsRow.appendChild(sleepLabelContainer)
 
-		// Data from helper
-		if (this.dataNotification) {
-			var wrapperDataNotification = document.createElement("div");
-			// translations  + datanotification
-			wrapperDataNotification.innerHTML =  this.translate("UPDATE") + ": " + this.dataNotification.date;
+		var sleepDurationLabelContainer = document.createElement("th")
+		var sleepDurationLabel = document.createTextNode("Duration")
+		sleepDurationLabelContainer.appendChild(sleepDurationLabel)
+		labelsRow.appendChild(sleepDurationLabelContainer)
 
-			wrapper.appendChild(wrapperDataNotification);
-		}
+		var table = document.createElement("table")
+		table.className = "oura-table"
+		table.appendChild(scoresRow)
+		table.appendChild(labelsRow)
+
+		var wrapper = document.createElement("div")
+		wrapper.appendChild(table)
 		return wrapper;
 	},
 
@@ -133,25 +85,5 @@ Module.register("MagicMirrorModule-Oura", {
 			en: "translations/en.json",
 			es: "translations/es.json"
 		};
-	},
-
-	processData: function(data) {
-		var self = this;
-		this.dataRequest = data;
-		if (this.loaded === false) { self.updateDom(self.config.animationSpeed) ; }
-		this.loaded = true;
-
-		// the data if load
-		// send notification to helper
-		this.sendSocketNotification("MagicMirrorModule-Oura-NOTIFICATION_TEST", data);
-	},
-
-	// socketNotificationReceived from helper
-	socketNotificationReceived: function (notification, payload) {
-		if(notification === "MagicMirrorModule-Oura-NOTIFICATION_TEST") {
-			// set dataNotification
-			this.dataNotification = payload;
-			this.updateDom();
-		}
 	},
 });
